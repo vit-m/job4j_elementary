67690e3269b50e0e4c99abca54899f07809f4d09
Vitaly Miroshkin <sysop14@gmail.com>
2021-08-22 21:49:12 +0200

    4.1.2. Глупый бот [#31323]

---
+++ src/ru/job4j/DummyBot.java
@@ -0,0 +1,21 @@
+package ru.job4j;
+
+public class DummyBot {
+    public static String answer(String question) {
+        String result = "Это ставит меня в тупик. Задайте другой вопрос.";
+        if ("Привет, Бот".equals(question)) {
+            result = "Привет, умник!";
+        } else if ("Пока".equals(question)) {
+            result = "До скорой встречи";
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        String result = DummyBot.answer("Привет, Бот");
+        System.out.println(result);
+
+        result = DummyBot.answer("Пока");
+        System.out.println(result);
+    }
+}
