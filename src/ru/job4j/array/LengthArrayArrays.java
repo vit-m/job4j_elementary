81565762beca0ea82b291d8cf70b4fb8a484af9f
Vitaly Miroshkin <sysop14@gmail.com>
2021-08-28 21:22:15 +0200

    Fix 6.6.8. Двухмерный массив. Таблица умножения. [#33491 #195127]

--- src/ru/job4j/array/LengthArrayArrays.java
+++ src/ru/job4j/array/LengthArrayArrays.java
@@ -3,7 +3,7 @@ package ru.job4j.array;
 public class LengthArrayArrays {
     public static void main(String[] args) {
         int[][] arrays = {{1}, {2, 3}, {4, 5, 6}, {7, 8, 9, 10}};
-        for (int i = 0; i < arrays.length; i += 1) {
+        for (int i = 0; i < arrays.length; i++) {
             System.out.println("Размер вложенного массива равен: " + arrays[i].length);
         }
     }
